ec5e608c4c1f179b79cd7474af7bbeebeaa0da2f
Igor Silva <igormiguel37@gmail.com>
2024-11-19 22:04:21 -0300

    First implementation of security and add spring AI in pom xml

---
+++ src/main/java/br/com/fiap/ecopontos/auth/Credentials.java
@@ -0,0 +1,5 @@
+package br.com.fiap.ecopontos.auth;
+
+public record Credentials(String email, String password) {
+
+}
---
+++ src/main/java/br/com/fiap/ecopontos/auth/Token.java
@@ -0,0 +1,5 @@
+package br.com.fiap.ecopontos.auth;
+
+public record Token(String token) {
+
+}
